55644853e3c2fe640b78282961bf749642f01f7c
yosrabenchikha <yossra.benchikha@gmail.com>
2026-08-30 20:38:45 +0100

    Initial commit

--- drift_simulator/feature_store/__init__.py
+++
@@ -1,6 +0,0 @@
-# Purpose: lightweight, config-driven feature store for the diabetes prediction project.
-
-from feature_store.registry import FeatureGroup, FeatureRegistry
-from feature_store.store import FeatureStore
-
-__all__ = ["FeatureStore", "FeatureRegistry", "FeatureGroup"]
